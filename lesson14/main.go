e1ec68c023a833eeaa20724725664a811d1d8c79
i-coder-robot <coder2998@gmail.com>
2022-05-24 21:43:02 +0800

    lesson14

--- lesson14/main.go
+++ lesson14/main.go
@@ -7,28 +7,29 @@ import (
 
 func main() {
 	/*
-		func functionName(parameterList){
-			//code
+		定义函数
+		function functionName(parameterList){
+			//业务逻辑代码
 		}
-		正常调用一个函数
+		调用函数
 		functionName(parameterList)
-		开启一个协程执行这个函数
+		开启一个协程调用函数
 		go functionName(parameterList)
 	*/
 
-	//开启一个协程执行PrintInfo()
-	go PrintInfo()
-	//使主协程休眠1秒
-	time.Sleep(1 * time.Second)
-	//打印main
-	fmt.Println("main...")
+	//go PrintInfo()
+	//
+	//time.Sleep(1 * time.Second)
+	//
+	//fmt.Println("main...")
 
-	//启动一号协程
+	//开启1号协程
 	go PrintNum(1)
-	//启动二号协程
+
+	//开启2号协程
 	go PrintNum(2)
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(time.Second)
 }
 
 func PrintNum(num int) {
@@ -39,5 +40,5 @@ func PrintNum(num int) {
 }
 
 func PrintInfo() {
-	fmt.Println("从0到Go语言微服务架构师")
+	fmt.Println("从0到GO语言微服务架构师")
 }
